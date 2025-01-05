f1f502786ff0d9138f69d332a166ecaee0b3fa6c
dvruette <dimitrivr@icloud.com>
2025-01-05 19:55:19 +0100

    update semantic_diffusion model, add multi-gpu support

--- lm_eval/models/semantic_diffusion.py
+++ lm_eval/models/semantic_diffusion.py
@@ -1,4 +1,7 @@
+import os
 import random
+import logging
+from typing import Literal
 
 from tqdm import tqdm
 
@@ -8,51 +11,80 @@ from typing import Union
 from lm_eval.utils import simple_parse_args_string
 
 import torch
+import torch.distributed as dist
 
-from semantic_diffusion.forward_process import get_noise_schedule
-from semantic_diffusion.utils import load_checkpoint, parse_dtype
-from semantic_diffusion.likelihood import Likelihood, compute_nll
+from semantic_diffusion.utils import parse_dtype
+from semantic_diffusion.loss import get_loss
+from semantic_diffusion.checkpoints import load_checkpoint
+from semantic_diffusion.likelihood import ELBO, compute_elbo
 
+
+logger = logging.getLogger(__name__)
 
 @register_model("semantic_diffusion")
 class SemanticDiffusion(LM):
-    def __init__(self, model_path: str, num_samples: int = 32, batch_size: int = 1) -> None:
+    def __init__(
+        self,
+        model_path: str,
+        num_samples: int = 32,
+        likelihood_method: Literal["loss", "elbo"] = "elbo",
+        device: Union[str, torch.device] | None = None,
+        batch_size: str | int = 1,
+        **kwargs,
+    ) -> None:
         # super init
         super().__init__()
 
         # attributes
         self.model_path = model_path
         self.num_samples = num_samples
-        self.batch_size = batch_size
+        self.batch_size = int(batch_size)
+        self.device = torch.device(device) if device is not None else torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+        if "LOCAL_RANK" in os.environ:
+            self._rank = int(os.environ["LOCAL_RANK"])
+            self._world_size = int(os.environ["WORLD_SIZE"])
+            dist.init_process_group(backend="nccl", rank=self.rank, world_size=self.world_size)
+            self.device = torch.device("cuda", self.rank)
 
         # print the model path
-        print(f"Loading model from {model_path}")
+        logger.info(f"[RANK: {self.rank}] Loading model from {model_path}")
 
-        # torch stuff
-        torch.set_float32_matmul_precision('high')
-        torch.set_grad_enabled(False)
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        # # torch stuff
+        # torch.set_float32_matmul_precision('high')
 
         # load the model
-        self.model, self.tokenizer, self.config = load_checkpoint(model_path, device=self.device)
-        self.model.eval()
+        model, noise_schedule, tokenizer, config = load_checkpoint(model_path, device=self.device)
+        model.eval()
+
+        self.config = config
+        self.model = model
+        self.tokenizer = tokenizer
+        self.dtype = parse_dtype(self.config.training.dtype)
 
         # parse the dtype the model was trained in
         self.dtype = parse_dtype(self.config.training.dtype)
 
-        # load the noise schedule
-        self.noise_schedule = get_noise_schedule(self.config, self.tokenizer)
-
         # construct the likelihood estimator
-        likelihood = Likelihood(self.config, self.model, self.noise_schedule).to(self.device)
+        loss_fn = get_loss(config, tokenizer, noise_schedule)
+        if likelihood_method == "elbo":
+            loss_fn.loss_weighting = True
+            loss_fn.our_weighting = False
+            loss_fn.min_loss_weight = -1e6
+            loss_fn.max_loss_weight = 1e6
+        likelihood = ELBO(config, model, noise_schedule, loss_fn)
+        likelihood = likelihood.to(self.device)
 
         # compile for better efficiency
         self.likelihood = torch.compile(likelihood)
 
-
     @classmethod
     def create_from_arg_string(cls, arg_string, additional_config=None):
-        args = simple_parse_args_string(arg_string)
+        logger.info(additional_config)
+        args = {}
+        if additional_config is not None:
+            args = additional_config
+        args.update(simple_parse_args_string(arg_string))
         return cls(**args)
 
     def loglikelihood(self, requests, disable_tqdm: bool = False):
@@ -61,26 +93,27 @@ class SemanticDiffusion(LM):
         # create the batches input
         strings = [r.args[0] + r.args[1] for r in requests]
         batched = [strings[i:i + self.batch_size] for i in range(0, len(strings), self.batch_size)] 
-        with torch.no_grad(), torch.autocast(self.device.type, self.dtype):            
-            for batch in tqdm(batched, disable=disable_tqdm):
-                # load a batch
-                batch = self.tokenizer(batch, return_tensors="pt", padding="max_length", truncation=True, max_length=self.config.model.max_seq_len)
-                batch = batch.to(self.device)
+        for batch in tqdm(batched, disable=disable_tqdm):
+            # load a batch
+            batch = self.tokenizer(batch, return_tensors="pt", padding="max_length", truncation=True, max_length=self.config.model.max_seq_len)
+            batch = batch.to(self.device)
 
-                metrics, token_nlls = compute_nll(
-                            self.likelihood,
-                            batch,
-                            num_samples=self.num_samples,  # number of inner samples in the estimator, higher number has lower bias and variance
-                            show_progress=False,  # turn on/off the progress bar
-                            return_token_nlls=True,  # set to True to return the token-level nlls
-                        )
+            with torch.no_grad(), torch.autocast(self.device.type, self.dtype):            
+                _, token_nlls = compute_elbo(
+                    self.likelihood,
+                    batch,
+                    num_samples=self.num_samples,  # number of inner samples in the estimator, higher number has lower bias and variance
+                    t_eps=self.config.model.get("t_eps", 1e-5),  # time epsilong for the noise schedule
+                    show_progress=False,  # turn on/off the progress bar
+                    return_token_nlls=True,  # set to True to return the token-level nlls
+                )
 
                 # shape of token_nlls: (batch_size, max_seq_len)
                 # also includes NLL for padding tokens, can be masked like this:
                 non_padding_nll = token_nlls * batch["attention_mask"] 
 
                 # get the NLL per sample
-                nll = non_padding_nll.sum(dim=-1)/batch["attention_mask"].sum(dim=-1)
+                nll = non_padding_nll.sum(dim=-1) / batch["attention_mask"].sum(dim=-1)
                 res.extend(nll.cpu().numpy())
         
         return res
@@ -101,3 +134,24 @@ class SemanticDiffusion(LM):
             res.append(-random.random())
 
         return res
+
+    @property
+    def accelerator(self):
+        return self._Accelerator(self.world_size)
+
+    class _Accelerator:
+        def __init__(self, world_size):
+            self.world_size = world_size
+
+        def wait_for_everyone(self):
+            dist.barrier()
+
+        def gather(self, local_tensor):
+            gathered_tensors = [
+                torch.zeros_like(local_tensor)
+                for _ in range(self.world_size)
+            ]
+            dist.all_gather(gathered_tensors, local_tensor)
+            if local_tensor.dim() < 1:
+                return torch.stack(gathered_tensors)
+            return torch.cat(gathered_tensors)
